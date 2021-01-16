bea30fef3bca275be73bcdbcbc9f60e4dd1eaf9a
SunghyunKim9744 <dqq9744@naver.com>
2021-01-17 08:44:19 +0900

    성현 : 프로젝트 생성

---
+++ src/main/java/com/ggiriggiri/web/GgiriGgiriApplication.java
@@ -0,0 +1,13 @@
+package com.ggiriggiri.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgiriGgiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgiriGgiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggiriggiri/web/GgiriGgiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggiriggiri.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgiriGgiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
