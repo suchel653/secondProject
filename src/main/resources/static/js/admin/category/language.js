7c3b562c664caff1c3420f96031d36414360587d
aengun <aengun@icloud.com>
2021-01-25 17:20:03 +0900

    건영 : 이미지 여러개 등록 가능하도록 변경

--- src/main/resources/static/js/admin/category/language.js
+++ src/main/resources/static/js/admin/category/language.js
@@ -8,22 +8,20 @@ window.addEventListener("load", (e) => {
 	// 추가 버튼 이벤트
 	addBtn.onclick = () => {
 		count++;
-		if(count > 1)
-			alert('두 개 이상은 안되지롱~!');
-		else{
-			let tr = `<tr>
-						  <td></td>
-		                  <td>
-		                    <input type="text" name="newNames">
-		                  </td>
-						  <td><input type="file" name="file" /></td>
-		                  <td></td>
-					  </tr>`;
-	
-			tbody.insertAdjacentHTML('afterbegin', tr);			
-		}
+
+		let tr = `<tr>
+					  <td></td>
+	                  <td>
+	                    <input type="text" name="newNames">
+	                  </td>
+					  <td><input type="file" name="file" /></td>
+	                  <td></td>
+				  </tr>`;
+
+		tbody.insertAdjacentHTML('afterbegin', tr);
+
 	};
-	
+
 	// 수정 이벤트 발생 시
 	tbody.addEventListener("change", function(e) {
 		if (e.target.name != "newNames" && e.target.name != "del" && e.target.name != "file") {
@@ -31,5 +29,5 @@ window.addEventListener("load", (e) => {
 			e.target.previousElementSibling.name = "changedIds";
 		}
 	});
-	
+
 });
